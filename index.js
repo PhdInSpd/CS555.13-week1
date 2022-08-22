abb12ef6575399d0014ead2773a8e0c19856fd70
Chaver Yadim <51681259+PhdInSpd@users.noreply.github.com>
2022-08-22 11:57:11 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// my first server side code
+let myHttp = require("http");
+
+// arguments
+// 1. fnc that will run on http requoest
+let myServer = myHttp.createServer(
+  
+  function( myRequest, myResponse) {
+    console.log( myRequest.url );
+
+    // writeHead() creates an http response header. including status code( 200 ok)
+    myResponse.writeHead( 200, 
+                           {"Context-Type": "text/plain"} );
+
+    // end() returns some data and cxloses the response (sends it)
+    myResponse.end( "Hello Everyone!\n");
+  }
+);
+
+// ask http to start listening on a tcp port for incoming http requests
+myServer.listen(
+                  8080,      //tcp port
+                  "0.0.0.0"  //ip address
+                );
+
+console.log("server has started")
